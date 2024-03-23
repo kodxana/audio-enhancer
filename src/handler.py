8154910b4381be18b8fd2a4bfe3df7b1b8433477
kodxana <romanrex19@int.pl>
2024-03-23 20:04:13 +0100

    refactored to output b64

--- src/handler.py
+++ src/handler.py
@@ -1,13 +1,15 @@
-# handler.py
-
 import os
 import random
 import torch
-from runpod.serverless.utils import rp_download, rp_upload
+import io
+import base64
+import numpy as np
+import soundfile as sf
+import runpod
+from runpod.serverless.utils import rp_download
 from runpod.serverless.utils.rp_validator import validate
 from audiosr import build_model, super_resolution
 from rp_schemas import INPUT_SCHEMA
-
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 torch.set_float32_matmul_precision("high")
@@ -20,19 +22,19 @@ def handler(job):
     
     if 'errors' in validated_input:
         return {"errors": validated_input['errors']}
-    
+
     validated_input = validated_input['validated_input']
     input_file_url = validated_input['input_file_url']
-    
+
     downloaded_file = rp_download.file(input_file_url)
     if downloaded_file["success"] is not True:
         return {"errors": "Failed to download the input file."}
-    
+
     input_file_path = downloaded_file["file_path"]
     seed = validated_input['seed'] or random.randint(0, 2**32 - 1)
     ddim_steps = validated_input['ddim_steps']
     guidance_scale = validated_input['guidance_scale']
-    
+
     waveform, sr = super_resolution(
         builtModelSr,
         input_file_path,
@@ -40,19 +42,18 @@ def handler(job):
         guidance_scale=guidance_scale,
         ddim_steps=ddim_steps
     )
-    
-    # Convert waveform to 16-bit and save to .wav file
-    out_wav_path = f"{job['id']}_out.wav"  # Include job ID in the output file name for uniqueness
+
+    # Convert waveform to 16-bit and prepare in-memory WAV file
     out_wav = (waveform[0] * 32767).astype(np.int16).T
-    sf.write(out_wav_path, data=out_wav, samplerate=sr)
-    
-    # Upload the output file and get a URL for download
-    output_url = rp_upload.file(out_wav_path, job['id'])
-    
-    # Optionally, clean up the local output file after uploading
-    os.remove(out_wav_path)
-    
-    return {"output_url": output_url}
+    audio_bytes = io.BytesIO()
+    sf.write(audio_bytes, data=out_wav, samplerate=sr, format='WAV')
+    audio_bytes.seek(0)
+
+    # Encode the WAV file to a base64 string
+    base64_audio = base64.b64encode(audio_bytes.read()).decode('utf-8')
+
+    # Return the base64 encoded audio with the appropriate data URI scheme
+    return f"data:audio/wav;base64,{base64_audio}"
 
 # Assuming runpod.serverless.start correctly defined elsewhere
 runpod.serverless.start({"handler": handler})
